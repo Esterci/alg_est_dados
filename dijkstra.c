1f41f2d304d3fda91d5b0732235b28f83321b584
Esterci <thiago.esterci@engenharia.ufjf.br>
2023-06-03 20:13:30 -0300

    consegui!

--- dijkstra.c
+++ dijkstra.c
@@ -34,7 +34,7 @@ void print_aresta(aresta *L);
 
 aresta *allocate_aresta(int dny, int dist);
 
-void free_object(aresta *x);
+void free_aresta(aresta *x);
 
 grafo *graph_search(grafo *L, int k);
 
@@ -48,36 +48,56 @@ void free_graph(grafo *x);
 
 void print_graph(grafo *g);
 
-void djikstra(grafo **G);
+void djikstra(grafo **G, grafo *init);
+
+void print_fastest_way(grafo *g);
 
 // Codigo principal
 
 int main(void)
 {
   int opc, chave;
-  int n_nos = 3;
-  int n_arestas = 2;
+  int n_nos = 7;
+  int n_arestas = 13;
   int no_origem = 1;
-  int no_final = 6;
+  int no_final = 7;
   grafo *head = NULL, *aux = NULL;
   aresta *aux2 = NULL;
 
+  // Declarando arestas
+
+  int aresta_matrix[13][3] = {
+      {1, 2, 1},
+      {1, 4, 3},
+      {1, 3, 1},
+      {2, 4, 1},
+      {2, 6, 6},
+      {3, 4, 2},
+      {3, 5, 3},
+      {3, 7, 6},
+      {4, 6, 4},
+      {4, 5, 2},
+      {5, 6, 4},
+      {5, 7, 5},
+      {6, 7, 4},
+  };
+
   // Inserindo nos no grafo
   printf("Inserindo nos...\n");
-  for (int i = n_nos; i >= 0; i--)
+  for (int i = 0; i < n_nos; i++)
   {
     node_insert(&head, allocate_graph(i + 1));
     printf("\tNo %d inserido.\n", i + 1);
   };
 
   // Inserindo arestas no grafo
-  printf("\nInserindo arestas...\n");
+  printf("\n\nInserindo arestas...\n");
   for (int i = 0; i < n_arestas; i++)
   {
-    aux = graph_search(head, 1);
+    aux = graph_search(head, aresta_matrix[i][0]);
     if (aux != NULL)
     {
-      aresta_insert(&aux->adjacentes, allocate_aresta(1, i + 1));
+      aresta_insert(&aux->adjacentes, allocate_aresta(aresta_matrix[i][1], aresta_matrix[i][2]));
       printf("\tAresta %d inserida.\n", i + 1);
     }
     else
@@ -86,9 +106,23 @@ int main(void)
     }
   };
 
+  // aplicando algoritmo de djikstra
+
+  djikstra(&head, graph_search(head, no_origem));
+
   // Mostrando grafo resultante
-  printf("O grafo resultante é:\n");
+
+  printf("\n\nO grafo resultante é:\n");
   print_graph(head);
+
+  printf("\n\nO melhor caminho é:\n");
+  print_fastest_way(head);
+
+  // limpando estruturas
+
+  free_graph(head);
+  free_graph(aux);
+  free_aresta(aux2);
 }
 
 // Definindo funcoes
@@ -173,31 +207,6 @@ void node_delete(grafo **G, grafo *x)
     x->next->prev = x->prev;
 }
 
-void djikstra(grafo **G)
-{
-  grafo *aux_graph;
-  aresta *aux_adj = (*G)->adjacentes;
-
-  while (aux_adj != NULL)
-  {
-    aux_graph = graph_search((*G), aux_adj->destiny);
-
-    int tempo_chegada = aux_adj->distance + (*G)->tempo;
-
-    if (tempo_chegada < aux_graph->tempo)
-    {
-      aux_graph->tempo = tempo_chegada;
-      aux_graph->pred = (*G)->key;
-    }
-
-    // salvar nó
-
-    aux_adj = aux_adj->next;
-  }
-
-  // checar nó de menor peso
-}
-
 grafo *allocate_graph(int k)
 {
   grafo *x = (grafo *)malloc(sizeof(struct nos));
@@ -228,7 +237,10 @@ void print_graph(grafo *g)
   grafo *auxg = g;
   while (auxg != NULL)
   {
-    printf("No: %i \n\t Arestas: ", auxg->key);
+
+    printf("No: %d\n\t Predecessor: %d", auxg->key, auxg->pred);
+    printf("\n\t Tempo ate o no: %d", auxg->tempo);
+    printf("\n\t Arestas: ");
 
     aresta *auxl = auxg->adjacentes;
     while (auxl != NULL)
@@ -239,4 +251,55 @@ void print_graph(grafo *g)
     printf("\n");
     auxg = auxg->next;
   }
+}
+
+void djikstra(grafo **G, grafo *init)
+{
+  grafo *aux_graph = NULL;
+  aresta *aux_adj = NULL;
+
+  if (init != NULL && init->final != 1)
+  {
+    int tempo_chegada = 0;
+    aux_adj = init->adjacentes;
+    // inicializando nó em análise
+
+    if (init->next == NULL)
+    {
+      init->tempo = 0;
+    }
+
+    init->final = 1;
+
+    while (aux_adj != NULL)
+    {
+      aux_graph = graph_search((*G), aux_adj->destiny);
+
+      tempo_chegada = aux_adj->distance + init->tempo;
+
+      if (tempo_chegada < aux_graph->tempo)
+      {
+        aux_graph->tempo = tempo_chegada;
+        aux_graph->pred = init->key;
+      }
+
+      aux_adj = aux_adj->next;
+    }
+    djikstra(G, init->prev);
+  }
+}
+
+void print_fastest_way(grafo *g)
+{
+  grafo *auxg = g;
+  while (auxg != NULL)
+  {
+    printf("No %d\t- Horario de chegada: %d \n", auxg->key, auxg->tempo);
+    printf("\t ^\n");
+    printf("\t/ \\\n");
+    printf("\t |\n");
+    printf("\t |\n");
+    auxg = graph_search(g,auxg->pred);
+  }
+  printf("\n\n");
 }
